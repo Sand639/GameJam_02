986c4e8e6b9cfd71cc3fae97742adf775b5f9e79
Hukuda910 <yuto.f10@gmail.com>
2026-05-23 14:31:35 +0900

    UI
    コインにスコアの追加と、燃料ゲージをプレイヤーを参照するようにしました

---
+++ GameJam/Assets/Scripts/UI/Gauge.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gauge : MonoBehaviour
+{
+    [Header("ゲージ画像")]
+    public Image fillImage;
+
+    private float maxValue = 100f;
+ 
+
+    void Update()
+    {
+        UpdateGauge();
+    }
+
+    //public void FuelDamege(float amount)
+    //{
+    //    currentValue -= amount;
+    //    currentValue = Mathf.Clamp(currentValue, 0, maxValue);
+    //}
+
+    void UpdateGauge()
+    {
+        float current = Player.GetEnergy();
+        float target = current / maxValue;
+
+        fillImage.fillAmount = Mathf.Lerp(
+           fillImage.fillAmount,
+           target,
+           Time.deltaTime * 5f
+       );
+    }
+}
