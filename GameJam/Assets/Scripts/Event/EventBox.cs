318aa4d17e15b171b434e3ba42dbb1b6029fbc2c
大槻　海斗 <yziku100@gmail.com>
2026-05-23 14:49:28 +0900

    修正

--- GameJam/Assets/Scripts/Event/EventBox.cs
+++ GameJam/Assets/Scripts/Event/EventBox.cs
@@ -2,25 +2,16 @@ using UnityEngine;
 
 public class EventBox : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        // ぶつかってきたのがPlayerだったら
-        if (other.CompareTag("Player"))
-        {
-            // シーン内にある FuelMiniGame スクリプトを探して起動する
-            PressMiniGame miniGame = FindFirstObjectByType<PressMiniGame>();
+    public void MiniGameStart()
+    {    
+        // シーン内にある FuelMiniGame スクリプトを探して起動する
+        PressMiniGame miniGame = FindFirstObjectByType<PressMiniGame>();
 
-            if (miniGame != null)
-            {
-                miniGame.StartMiniGame();
+        miniGame.StartMiniGame();
 
-                // イベントボックス自身は役割を終えたので消す
-                Destroy(gameObject);
-            }
-            else
-            {
-                Debug.LogError("FuelMiniGame スクリプトがシーンに見つかりません！");
-            }
-        }
+        // イベントボックス自身は役割を終えたので消す
+        Destroy(gameObject);
+
     }
+
 }
