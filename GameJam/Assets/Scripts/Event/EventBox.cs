45eae3b749f2f17202f752023aa4779407e723bd
HAL-Kubodera <ths30024@ths.hal.ac.jp>
2026-05-23 15:08:43 +0900

    Revert "修正"
    This reverts commit 318aa4d17e15b171b434e3ba42dbb1b6029fbc2c.

--- GameJam/Assets/Scripts/Event/EventBox.cs
+++ GameJam/Assets/Scripts/Event/EventBox.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class EventBox : MonoBehaviour
 {
-    public void MiniGameStart()
-    {    
-        // シーン内にある FuelMiniGame スクリプトを探して起動する
-        PressMiniGame miniGame = FindFirstObjectByType<PressMiniGame>();
+    private void OnTriggerEnter(Collider other)
+    {
+        // ぶつかってきたのがPlayerだったら
+        if (other.CompareTag("Player"))
+        {
+            // シーン内にある FuelMiniGame スクリプトを探して起動する
+            PressMiniGame miniGame = FindFirstObjectByType<PressMiniGame>();
 
-        miniGame.StartMiniGame();
+            if (miniGame != null)
+            {
+                miniGame.StartMiniGame();
 
-        // イベントボックス自身は役割を終えたので消す
-        Destroy(gameObject);
-
+                // イベントボックス自身は役割を終えたので消す
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.LogError("FuelMiniGame スクリプトがシーンに見つかりません！");
+            }
+        }
     }
-
 }
